47fa636876ef13236dceb8437f4c0b8b2cb5f03f
Sucharitha <sucharitha.g@walkingtree.tech>
2018-04-19 16:55:22 +0530

    category filter changes

--- src/pages/feature/category-filters/category-filters.component.ts
+++ src/pages/feature/category-filters/category-filters.component.ts
@@ -1,8 +1,5 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { AppLitteralsConfig } from './../../../providers/literals/app.literals';
 import { AppointmentInfoService } from './../../../providers/services/appointment-info-service';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-category-filters',
@@ -20,50 +17,56 @@ export class CategoryFiltersComponent implements OnInit {
     genderSearchTerm: string;
     languageSearchTerm: string;
     public medicalArr = [];
-    public genderArr = []; locArr = []; languageArr = [];
+    public genderArr = []; public locArr = []; public languageArr = [];
 
-    searchForm: FormGroup
 
     languages: object[] = [{ id: 'en', name: 'English' }, { id: 'tel', name: 'Telugu' }, { id: 'fr', name: 'french' }, { id: 'kn', name: 'kannada' }];
     genders: object[] = [{ id: 'male', name: 'Male' }, { id: 'female', name: 'Female' }];
 
-    constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private appointmentInfo: AppointmentInfoService) {
+    constructor(private appointmentInfo: AppointmentInfoService) {
     }
 
     ngOnInit() {
-        this.initialisefields();
         this.locations = this.locations.length > 0 ? this.locations : this.appointmentInfo.getLocations();
         this.medicalCenters = this.medicalCenters.length > 0 ? this.medicalCenters : this.appointmentInfo.getHospitals();
-    }
-
-    initialisefields() {
-        this.searchForm = this.formBuilder.group({
-            "medical": [null],
-            "gender": [null],
-            "location": [null],
-            "language": [null]
-        });
     }
 
     filterData(event, type, value) {
         if (event.target.checked) {
             if (type == 'medical') {
                 this.medicalArr.push(value);
-                this.searchForm.controls[type].setValue(this.medicalArr);
             } else if (type == 'gender') {
                 this.genderArr.push(value);
-                this.searchForm.controls[type].setValue(this.genderArr);
             } else if (type == 'location') {
                 this.locArr.push(value);
-                this.searchForm.controls[type].setValue(this.locArr);
             } else {
                 this.languageArr.push(value);
-                this.searchForm.controls[type].setValue(this.languageArr);
             }
         } else {
-            
+            if (type == 'medical') {
+                let i = this.medicalArr.indexOf(value);
+                if (i > -1) {
+                    this.medicalArr.splice(i, 1);
+                }
+            }
+            else if (type == 'gender') {
+                let i = this.genderArr.indexOf(value);
+                if (i > -1) {
+                    this.genderArr.splice(i, 1);
+                }
+            } else if (type == 'location') {
+                let i = this.locArr.indexOf(value);
+                if (i > -1) {
+                    this.locArr.splice(i, 1);
+                }
+            } else {
+                let i = this.languageArr.indexOf(value);
+                if (i > -1) {
+                    this.languageArr.splice(i, 1);
+                }
+            }
         }
-        console.log(this.searchForm.value);
-       // this.filterItems.emit(this.searchForm.value);
+        let obj = { 'medical': this.medicalArr, 'gender': this.genderArr, 'location': this.locArr, 'language': this.languageArr };
+        this.filterItems.emit(obj);
     }
 }
--- src/pipes/category-filter-pipe.ts
+++ src/pipes/category-filter-pipe.ts
@@ -6,24 +6,22 @@ export class CategoryFilterPipe implements PipeTransform {
     transform(items: any, medicalSearch?: string[], locSearch?: string[], langSearch?: string[], genderSearch?: string[]) {
         if (items) {
             return items.filter(item => {
-                for (let i in medicalSearch) {
-                    if (medicalSearch && medicalSearch[i] && item.name && item.name.toLowerCase().indexOf(medicalSearch[i].toLowerCase()) === -1) {
-                        return false;
-                    }
+                if (medicalSearch) {
+                    let medical = medicalSearch.some(medical => medical == item.name);
+                    return medical;
                 }
-                for (let i in locSearch) {
-                    if (locSearch && locSearch[i] && item.locations && item.locations.landmark && item.locations.landmark.toLowerCase().indexOf(locSearch[i].toLowerCase()) === -1) {
-                        return false;
-                    }
+                if (locSearch) {
+                    let loc = locSearch.some(loc => loc == item.data[0].locations.landmark);
+                    return loc;
                 }
-                for (let i in langSearch) {
-                    if (langSearch && langSearch[i] && item.language && item.language.toLowerCase().indexOf(langSearch[i].toLowerCase()) === -1) {
-                        return false;
-                    }
+                if (langSearch) {
+                    let lang = langSearch.some(lang => lang == item.data[0].language);
+                    return lang;
                 }
-                // if (genderSearch && item.company.toLowerCase().indexOf(genderSearch.toLowerCase()) === -1) {
-                //     return false;
-                // }
+                if (genderSearch) {
+                    let gender = genderSearch.some(gender => gender == item.data[0].gender);
+                    return gender;
+                }
                 return true;
             })
         }
