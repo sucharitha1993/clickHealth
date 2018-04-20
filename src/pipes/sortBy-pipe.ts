eb1170c0730b8e3bee7e99c3d3d0fa5db4c41be2
Sucharitha <sucharitha.g@walkingtree.tech>
2018-04-20 12:23:55 +0530

    changed filter to main array

--- src/pipes/sortBy-pipe.ts
+++ src/pipes/sortBy-pipe.ts
@@ -4,20 +4,24 @@ export class SortByPipe implements PipeTransform {
 
     transform(records: Array<any>, arg?: any, direction?: any): any {
         return records.sort((a, b) => {
-            if (a[arg] < b[arg]) {
-                return -1 * direction;
-            }
-            else if (a[arg] > b[arg]) {
-                return 1 * direction;
-            }
-            else {
-                return 0;
+            for (let i in a.data) {
+                for (let j in b.data) {
+                    if (a.data[i][arg] < b.data[j][arg]) {
+                        return -1 * direction;
+                    }
+                    else if (a.data[i][arg] > b.data[j][arg]) {
+                        return 1 * direction;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
             }
         });
     };
 }
 
-/* for multiple Arr data
+/* for Simple Arr data
         return records.sort((a, b) => {
             for (let i in a.data) {
                 for (let j in b.data) {
