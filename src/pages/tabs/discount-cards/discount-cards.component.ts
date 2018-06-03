64678efaeb2b248003ab473874771022a0d1b51d
Sucharitha <sucharitha.g@walkingtree.tech>
2018-06-03 10:11:17 +0530

    Discount cards binding

--- src/pages/tabs/discount-cards/discount-cards.component.ts
+++ src/pages/tabs/discount-cards/discount-cards.component.ts
@@ -1,17 +1,19 @@
+import { CaroselService } from './../../../providers/services/carosel-service';
 import { DiscountDataService } from './../../../providers/services/discounts/dc-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-discount-cards',
-    templateUrl: './discount-cards.component.html'
+    templateUrl: './discount-cards.component.html',
+    styleUrls: ['./discount-cards.component.css']
 })
 export class DiscountCardsComponent implements OnInit {
 
 
     imgPrePath: string;
-    dcData: any;
+    dcData: any= [];
 
-    constructor(public dcService: DiscountDataService) {
+    constructor(public dcService: DiscountDataService, public CaroselService: CaroselService) {
         this.imgPrePath = '../../assets/img/';
     }
 
@@ -19,12 +21,16 @@ export class DiscountCardsComponent implements OnInit {
         this.loadDC();
     }
 
+    ngAfterViewInit() {
+        this.CaroselService.loadOwlCarousel();
+    }
+
     loadDC() {
         this.dcService.getDiscounts()
             .subscribe(res => {
-                if (res.status) {
-                    res.data = res.data || {};
-                    this.dcData = res.data;
+                if (res.results) {
+                    res.results = res.results || [];
+                    this.dcData = res.results;
                 }
             },
             error => {
