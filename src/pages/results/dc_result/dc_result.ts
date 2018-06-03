3de31222ce79af7d3f6a63a8b9d5b5f4d6dc6ea0
Sucharitha <sucharitha.g@walkingtree.tech>
2018-06-03 12:19:15 +0530

    DC url

--- src/pages/results/dc_result/dc_result.ts
+++ src/pages/results/dc_result/dc_result.ts
@@ -1,3 +1,4 @@
+import { AppConfig } from './../../../providers/services/app-config.service';
 import { SharingService } from './../../../providers/services/sharing-service';
 import { DCInfoService } from './../../../providers/services/discounts/dc-info.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DCResultComponent implements OnInit {
 
-
+    AppConfig: AppConfig;
     imgPrePath: string;
     dcData: any = [];
     activeDiscount: any = {};
+    url: any;
 
     constructor(public dcInfo: DCInfoService, public sharingService: SharingService) {
         this.imgPrePath = '../../assets/img/';
@@ -22,11 +24,13 @@ export class DCResultComponent implements OnInit {
         this.dcData[0] = this.dcData[0] || {};
         this.dcData[0].activeClass = true;
         this.activeDiscount = this.dcData[0];
+        this.url = `${AppConfig.IMG_URL}healthseeker/discountcard_get/?card_id=${this.activeDiscount.id}`;
     }
 
     checkActiveClass(data) {
         data.activeClass == !data.activeClass == undefined ? true : !data.activeClass;
         this.activeDiscount = data;
+        this.url = `${AppConfig.IMG_URL}healthseeker/discountcard_get/?card_id=${this.activeDiscount.id}`;
     }
 
 }
