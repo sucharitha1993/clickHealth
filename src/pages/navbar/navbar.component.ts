1fd769114ce19b3828f089942df9d9998f1d927a
Sucharitha <sucharitha.g@walkingtree.tech>
2018-06-03 12:55:45 +0530

    login/ signup dropdowns & navigations

--- src/pages/navbar/navbar.component.ts
+++ src/pages/navbar/navbar.component.ts
@@ -1,14 +1,20 @@
+import { AppConfig } from './../../providers/services/app-config.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
     selector: 'app-navbar',
-    templateUrl: './navbar.component.html'
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-
+    AppConfig: AppConfig;
     imgPrePath: string;
+    healthSeekerSigin: any = `${AppConfig.API_ENDPOINT}healthseeker/signin`;
+    healthProviderSignin: any = `${AppConfig.API_ENDPOINT}healthprovider/signin`;
+    clinicianSignin: any = `${AppConfig.API_ENDPOINT}clinician/signin`;
+    sponsorSignin: any = `${AppConfig.API_ENDPOINT}sponsor/signin`;
+    adminSignin: any = `${AppConfig.API_ENDPOINT}site_admin/signin`;
 
     constructor(private router: Router) {
         this.imgPrePath = '../../assets/img/';
